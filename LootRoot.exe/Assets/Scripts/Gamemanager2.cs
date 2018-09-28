0151b2d58a840daf1796e0578859c7f9c0953cce
Manninen Ossi TTK17SP <ossimanninen1@kamk.fi>
2018-09-28 20:57:22 +0300

    manager

---
+++ LootRoot.exe/Assets/Scripts/Gamemanager2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class Gamemanager2 : MonoBehaviour {
+
+    public static Gamemanager2 instance = null;
+
+    int triesLeft = 3;
+    int score = 0;
+
+    public int levels;
+    int currentLevel = 1;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            NextLevel();
+        }
+
+    }
+
+    void NextLevel()
+    {
+        SceneManager.LoadScene("level"+currentLevel);
+        currentLevel += 1;
+    }
+
+}
