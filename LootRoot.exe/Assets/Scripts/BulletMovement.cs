c0a3cab718e904642528dc32e20d4e93ea3140ed
Myllylä Juuso TTK17SP <juusomyllyla1@kamk.fi>
2018-09-28 20:44:56 +0300

    Okay I guess this is something

---
+++ LootRoot.exe/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+    float speed;
+    public GameObject shooting;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = .1f;        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (gameObject.name == "UpBullet(Clone)")
+        {
+            transform.position += transform.up * speed;
+        }
+        else if (gameObject.name == "DownBullet(Clone)")
+        {
+            transform.position -= transform.up * speed;
+        }
+        else if (gameObject.name == "RightBullet(Clone)")
+        {
+            transform.position += transform.right * speed;
+        }
+        else if (gameObject.name == "LeftBullet(Clone)")
+        {
+            transform.position -= transform.right * speed;
+        }
+
+
+    }
+}
---
+++ LootRoot.exe/Assets/Scripts/Shooting.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    float timer;
+    const float reset = 2;
+    public GameObject upBullet;
+    public GameObject downBullet;
+    public GameObject rightBullet;
+    public GameObject leftBullet;
+
+    public enum Direction { up, down, left, right };
+    public Direction direction;
+
+    GameObject spawnObject;
+
+    // Use this for initialization
+    void Start ()
+    {
+        timer = reset;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timer -= .1f;
+        
+        if (timer <= 0)
+        {
+            if (direction == Direction.up)
+            {
+                spawnObject = upBullet;
+            }
+            else if (direction == Direction.down)
+            {
+                spawnObject = downBullet;
+            }
+            else if (direction == Direction.left)
+            {
+                spawnObject = leftBullet;
+            }
+            else if (direction == Direction.right)
+            {
+                spawnObject = rightBullet;
+            }
+
+            Instantiate(spawnObject);
+            spawnObject.transform.position = transform.position;
+            timer = reset;
+        }
+	}
+}
