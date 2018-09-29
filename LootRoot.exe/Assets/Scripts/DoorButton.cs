f2cca9d1665fa5836fe2db7079f61b9b266b2436
Siukola Henry TTK17SP <henrysiukola@kamk.fi>
2018-09-29 12:25:35 +0300

    Button that opens doors aka destorys them. Also door

---
+++ LootRoot.exe/Assets/Scripts/DoorButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour {
+    public List<GameObject> doors = new List<GameObject>();
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            if (doors != null)
+            {
+                foreach(GameObject door in doors)
+                {
+                    Destroy(door);
+                }
+                gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+            }
+        }
+    }
+}
