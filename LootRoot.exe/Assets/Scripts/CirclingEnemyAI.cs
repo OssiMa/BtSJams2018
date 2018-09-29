c6ee7c691aea3a9f6b2ffe7a419dfc380b64d184
Siukola Henry TTK17SP <henrysiukola@kamk.fi>
2018-09-29 17:01:32 +0300

    Circling enemy

---
+++ LootRoot.exe/Assets/Scripts/CirclingEnemyAI.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class CirclingEnemyAI : MonoBehaviour
+{
+    public Vector2 Velocity = new Vector2(1, 0);
+
+
+    public float RotateSpeed = 1f;
+
+    public float Radius = 1f;
+
+    private Vector2 _centre;
+    private float _angle;
+    public bool moving;
+
+    public CirclingDirection currentCirclingDirection = CirclingDirection.Clockwise;
+    public MovingDirection currentMovingDirection = MovingDirection.Forward;
+    private void Start()
+    {
+        _centre = transform.position;
+    }
+
+    private void Update()
+    {
+        if (moving)
+        {
+            _centre += Velocity * Time.deltaTime * (int)currentMovingDirection;
+        }
+
+        _angle += RotateSpeed * Time.deltaTime * (int)currentCirclingDirection;
+
+        var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * Radius;
+
+        transform.position = _centre + offset;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            collision.gameObject.GetComponent<ThePlayerMovement>().KillPlayer();
+        }
+        else
+        {
+            switch(currentCirclingDirection)
+            {
+                case CirclingDirection.Clockwise:
+                    currentCirclingDirection = CirclingDirection.CounterClockwise;
+                    break;
+                case CirclingDirection.CounterClockwise:
+                    currentCirclingDirection = CirclingDirection.Clockwise;
+                    break;
+            }
+
+        }
+    }
+
+    public enum CirclingDirection
+    {
+        Clockwise = 1,
+        CounterClockwise = -1
+    }
+    public enum MovingDirection
+    {
+        Forward = 1,
+        BackWard = -1
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(_centre, 0.1f);
+        Gizmos.DrawLine(_centre, transform.position);
+    }
+}
