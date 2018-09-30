36802b9dce499153a833221ad765fc7bfdac9e4b
ramitammela2 <37778027+ramitammela2@users.noreply.github.com>
2018-09-30 16:06:39 +0300

    Things

---
+++ LootRoot.exe/Assets/MatrixEffect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MatrixEffect : MonoBehaviour
+{
+    public float minX;
+    public float maxX;
+    public GameObject[] prefab;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("Update2", 0, 0.01f);
+	}
+
+    // Update is called once per frame
+    public void Update2()
+    {
+        
+        GameObject prefabCopy = Instantiate<GameObject>(prefab[Random.Range(0,prefab.Length)]);
+
+        prefabCopy.transform.position = new Vector2(Random.Range(minX, maxX), 8);
+        prefabCopy.transform.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+
+        float speed = Random.Range(-120, 120); 
+        if (speed > -30 && speed < 0)
+        {
+            speed = -30;
+        }
+        if (speed < 30 && speed >= 0)
+        {
+            speed = 30;
+        }
+        prefabCopy.transform.GetComponent<Rotate>().rotateSpeed = speed;
+
+
+        GameObject.Destroy(prefabCopy, 10f);
+
+        
+    }
+}
